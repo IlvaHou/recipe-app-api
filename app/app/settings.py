a83d37507598198514e94c45daef71f284cada6c
Ilva <i.van.houwelingen@hollandptc.nl>
2026-01-08 17:06:14 +0100

    Configure swagger documentation page

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'rest_framework'
-    'drf-spectacular'
+    'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
